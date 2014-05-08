8b0739a636b5d055a47f17bd5404d0811888faf6
Daniele Campogiani <daniele.campogiani@gmail.com>
2014-05-08 19:52:17 +0200

    fix: add properties to markers not pictures + remove click on markers

--- app/scripts/controllers/PlacesController.js
+++ app/scripts/controllers/PlacesController.js
@@ -1,13 +1,6 @@
 angular.module('MyPhotoDiary.controllers')
 
-    .controller('PlacesController', function($scope, $ionicPopup, $ionicModal, GeolocationService, PicturesService){
-
-        $ionicModal.fromTemplateUrl('templates/singlePhoto.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function(modal) {
-            $scope.modal = modal;
-        });
+    .controller('PlacesController', function($scope, $ionicPopup, GeolocationService, PicturesService){
 
         var _mapOptions = {keyboardShortcuts: false, panControl: false, zoomControl: false, streetViewControl: false};
 
@@ -37,64 +30,18 @@ angular.module('MyPhotoDiary.controllers')
                 });
             });
 
+        var _markers = [];
+
         var _pictures = PicturesService.getAllPictures();
 
         var _len = _pictures.length;
 
         for(var i=0;i<_len;i++){
-            _pictures[i].options = {animation: google.maps.Animation.DROP, icon: { url: _pictures[i].url, scaledSize: { height: 100, width: 100} } };
+            var _marker = {latitude: _pictures[i].latitude, longitude: _pictures[i].longitude, options: {icon: { url: _pictures[i].url, scaledSize: { height: 75, width: 75} } } };
+            _markers.push(_marker);
         }
 
-        $scope.pictures = _pictures;
-
-        $scope.onPictureSelected = function($markerModel){
-            //alert($markerModel.description);
-
-            $scope.selectedPicture = $markerModel;
-            $scope.modal.show();
-
-            //$state.go('tabs.timeline');
-        };
-
-        $scope.deletePhoto = function(toDelete) {
-
-            var _confirmPopup = $ionicPopup.confirm({
-                title: 'Please confirm',
-                template: 'Are you sure you want to delete '+toDelete.description+'?',
-                okText: 'Delete',
-                okType: 'button-assertive'
-            });
-
-            _confirmPopup.then(function(res) {
-                if(res) {
-                    PicturesService.deletePicture(toDelete).then(
-
-                        function(result){
-                            //TODO remove from map
-                            //$scope.pictures.splice(index,1);
-
-                            $scope.modal.hide();
-
-                        },
-                        function(error){
-                            $ionicPopup.alert({
-                                title: 'Error',
-                                content: error,
-                                okType: 'button-assertive'
-                            }).then(function(res) {
-                            });
-                        });
-                }
-            });
-
-        };
-
-
-        $scope.sharePhoto = function(toShare) {
-
-            window.plugins.socialsharing.share(null, null, toShare.url, null);
-            $scope.modal.hide();
-        };
+        $scope.markers = _markers;
 
 
         $scope.$on('$destroy', function() {
